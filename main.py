7e43d2f484410e86e15b956a5600ea6b811c26d0
priyanshu <priyanshu@shorthills.ai>
2024-12-09 23:00:22 +0530

    Extracted valueable insight from the pdf

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import ollama
+from PyPDF2 import PdfReader
 
+
+
+file_path = "FY24_Q1_Consolidated_Financial_Statements.pdf"
+reader = PdfReader(file_path)
+
+text1 = """lease find the value insight in the text and if that insight is not present kindly print the empty string . Output would be like that 
+        insight1 : value1
+        insight2 : value2
+        insight3 : value2
+        .......
+
+        insight value are == net sales, total cost of sales , total operating expense,net income from 
+"""
 
 
 def gen_answer(text):
@@ -14,3 +28,23 @@ def gen_answer(text):
 print("Enter the Question You have in precised way to get better output")
 b = input();
 gen_answer(b);
+
+
+def pdf_insight(insigt):
+    response = ollama.chat(model='llama3.1', messages=[
+    {
+    'role': 'user',
+    'content': insigt,
+    },
+    ])
+    print(response['message']['content'])
+
+text2="";
+for page in reader.pages:
+    text2+=page.extract_text()
+    text2+=" "
+
+prompt = text1+text2;
+
+pdf_insight(prompt);
+
